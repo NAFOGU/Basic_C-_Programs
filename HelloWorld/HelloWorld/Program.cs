f6f7ab7104bd91f536d9b672f8b58a673dfa5217
Nafisat Ogunleye <146068605+NAFOGU@users.noreply.github.com>
2025-06-19 11:14:04 -0400

    Initial commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("\"Hello, World!\" ");
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Remember, we\'re \"accounting\" on you!");
+        Console.ReadLine();
+    }
+}
---
+++ SampleApplication.cs
@@ -0,0 +1,9 @@
+using System;
+
+class SampleApplication
+{
+    static void Main()
+    {
+        console.WriteLine("Hello, World");
+    }
+}
