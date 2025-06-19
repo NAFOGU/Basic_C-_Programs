9aa4e3a430d9704af687c8bc5a6d462884971c30
Nafisat Ogunleye <146068605+NAFOGU@users.noreply.github.com>
2025-06-19 14:38:25 -0400

    Commit

---
+++ DailyReport/DailyReport/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Academy of Learning Career College");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string yourCourse = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            int pageNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            string help = Console.ReadLine();
+            bool helpBool = bool.Parse(help);
+            Console.WriteLine("Were there any positive experiences you\'d like to share? Please give specifics");
+            string specifics = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you\'d like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            int hours = int.Parse(Console.ReadLine());
+            Console.WriteLine("Thank you for your answers. An instructor will respond" +
+                " shortly. Have a great day!");
+            Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is " + total);
+            //Console.ReadLine();
+        }
+    }
+}
