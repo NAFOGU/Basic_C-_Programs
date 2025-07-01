4efbcdff9b39e648e1820b4030886d29d128debb
Nafisat Ogunleye <146068605+NAFOGU@users.noreply.github.com>
2025-07-01 05:02:44 -0400

    commit

--- DailyReport/DailyReport/Program.cs
+++ DailyReport/DailyReport/Program.cs
@@ -10,6 +10,7 @@ namespace DailyReport
     {
         static void Main(string[] args)
         {
+            //This is a program to create a daily report for students of AOLCC
             Console.WriteLine("Academy of Learning Career College");
             Console.WriteLine("Student Daily Report");
             Console.WriteLine("What is your name?");
@@ -17,26 +18,20 @@ namespace DailyReport
             Console.WriteLine("What course are you on?");
             string yourCourse = Console.ReadLine();
             Console.WriteLine("What page number?");
-            int pageNum = int.Parse(Console.ReadLine());
+            int pageNum = int.Parse(Console.ReadLine()); //Casts response as integer
             Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
             string help = Console.ReadLine();
-            bool helpBool = bool.Parse(help);
+            bool helpBool = bool.Parse(help); // casts string response as boolean
             Console.WriteLine("Were there any positive experiences you\'d like to share? Please give specifics");
             string specifics = Console.ReadLine();
             Console.WriteLine("Is there any other feedback you\'d like to provide? Please be specific.");
             string feedback = Console.ReadLine();
             Console.WriteLine("How many hours did you study today?");
-            int hours = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine()); // casts response as integer
             Console.WriteLine("Thank you for your answers. An instructor will respond" +
                 " shortly. Have a great day!");
             Console.ReadLine();
 
-            //Console.WriteLine("What is your favorite number?");
-            //string favoriteNumber = Console.ReadLine();
-            //int favNum = Convert.ToInt32(favoriteNumber);
-            //int total = favNum + 5;
-            //Console.WriteLine("Your favorite number plus 5 is " + total);
-            //Console.ReadLine();
         }
     }
 }
