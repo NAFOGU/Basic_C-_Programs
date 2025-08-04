d46856fc754724389e81ca2787b3ae66a1197fe2
Nafisat Ogunleye <146068605+NAFOGU@users.noreply.github.com>
2025-08-04 06:28:09 -0400

    commit

---
+++ MethodClassAssignement/MethodClassAssignement/MathClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MathClass
+{
+    // Void method taking two integer parameters
+    public void DoMath(int number1, int number2)
+    {
+        int result = number1 * 2; // Do a math operation on the first number
+        Console.WriteLine("Result of operation on first number: " + result);
+        Console.WriteLine("Second number is: " + number2);
+    }
+}
---
+++ MethodClassAssignement/MethodClassAssignement/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the class
+            MathClass math = new MathClass();
+
+            // Call the method with positional arguments
+            math.DoMath(5, 10);
+
+            Console.WriteLine();
+
+            // Call the method with named arguments
+            math.DoMath(number1: 8, number2: 15);
+
+            Console.ReadLine();
+        }
+    }
+}
